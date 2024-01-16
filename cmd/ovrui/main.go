4388b8338ecb4e208891a201ea652a845296e01d
Fabrice Aneche <akh@inair.space>
2024-01-15 22:30:15 -0500

    ui update

--- cmd/ovrui/main.go
+++ cmd/ovrui/main.go
@@ -2,99 +2,215 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"os"
+	"strings"
 
 	"github.com/AllenDang/giu"
 	g "github.com/AllenDang/giu"
+	"github.com/akhenakh/ovr/action"
+	"golang.design/x/clipboard"
 )
 
-var (
+var smallFont *g.FontInfo
+
+type App struct {
+	in                    []byte
+	out                   *action.Data
+	r                     *action.ActionRegistry
 	editor                *g.CodeEditorWidget
 	statusMsg             string
+	dataMsg               string
+	listBox               *g.ListBoxWidget
 	disableGlobalShortcut bool // disable global shortcut while using editor or /
 	visibleWidgets        []g.Widget
-	smallFont             *g.FontInfo
-)
-
-func onClickMe() {
-	fmt.Println("\ueb28 Hello world!")
+	searchInput           string
 }
 
-func onImSoCute() {
-	fmt.Println("Im sooooooo cute!!")
-}
-
-func loop() {
-	g.SingleWindow().RegisterKeyboardShortcuts(
-		// quit command
-		giu.WindowShortcut{Key: giu.KeyQ, Callback: func() {
-			if !disableGlobalShortcut {
-				os.Exit(0)
-			}
-		}},
-		// search command
-		giu.WindowShortcut{Key: giu.KeySlash, Callback: func() {
-			if !disableGlobalShortcut {
-				os.Exit(0)
-			}
-		}},
-		// view command
-		giu.WindowShortcut{Key: giu.KeyV, Callback: func() {
-			if !disableGlobalShortcut {
-				statusMsg = "ESC quit the editor"
-
-				visibleWidgets = []g.Widget{
-					g.Column(
-						g.Row(
-							g.Label(statusMsg).Font(smallFont),
-						),
-						editor,
-					),
-				}
-			}
-		}},
-		// close editor
-		giu.WindowShortcut{Key: giu.KeyEscape, Callback: func() {
-			if !disableGlobalShortcut {
-				os.Exit(0)
-			}
-		}},
-	).Layout(visibleWidgets...)
-}
-
-func main() {
-	wnd := g.NewMasterWindow("Hello world", 640, 480, g.MasterWindowFlagsFrameless)
+func newApp(in []byte) *App {
+	out := action.NewDataText(in)
 
 	smallFont = g.Context.FontAtlas.AddFont("iosevskanerdfont.ttf", 15)
 
 	g.Context.FontAtlas.SetDefaultFont("iosevskanerdfont.ttf", 24)
 
-	editor = g.CodeEditor().
+	editor := g.CodeEditor().
 		ShowWhitespaces(false).
-		TabSize(2).
-		Text("select * from greeting\nwhere date > current_timestamp\norder by date").
-		Border(true)
+		Text("").
+		Border(true).LanguageDefinition(giu.LanguageDefinitionC)
 
-	statusMsg = "q quit, v view"
-	visibleWidgets = []g.Widget{
-		g.Column(
-			g.Row(
-				g.Label(statusMsg).Font(smallFont),
+	statusMsg := "q quit, v view, / search"
+
+	r := action.DefaultRegistry()
+
+	// Make initial list of items
+	actions := r.ActionsForData(out)
+	items := make([]string, len(actions))
+	for i := 0; i < len(actions); i++ {
+		items[i] = actions[i].Title()
+	}
+	listBox := g.ListBox("actionList", items).Size(g.Auto, -20)
+
+	visibleWidgets := []g.Widget{
+		g.Row(g.Style().
+			SetColor(g.StyleColorText, color.RGBA{0x11, 0xDD, 0x11, 255}).
+			To(
+				g.Label("TEXT"),
 			),
-			g.ListBox("actionList", []string{"Jwt Parse", "Base64 Decode", "aaaaa", "bbbbb", "cccccc", "dddddd", "eeeeee", "ffffff", "aaaaa", "bbbbb", "cccccc", "dddddd", "eeeeee"}),
-			// g.Style().
-			// 	SetStyle(g.StyleVarFramePadding, 10, -30).
-			// 	SetFontSize(90).To(
-			// 	g.Button(string(0xe342)),
-			// ),
-			//
-			// 	g.Button("Click Me").OnClick(onClickMe),
-			// 	g.Button("I'm so cute").OnClick(onImSoCute),
-			// 	g.Button("salut").OnClick(func() { fmt.Println("yo") }),
-			// ),
+			g.Label(string(in)).Font(smallFont)),
+		listBox,
+		g.Label(statusMsg).Font(smallFont),
+		// g.Column(
+		// 	// g.Row(
+		// 	// 	g.Label(statusMsg).Font(smallFont),
+		// 	// ),
 
-		),
+		// ),
+
+		// g.Style().
+		// 	SetStyle(g.StyleVarFramePadding, 10, -30).
+		// 	SetFontSize(90).To(
+		// 	g.Button(string(0xe342)),
+		// ),
+		//
+		// 	g.Button("Click Me").OnClick(onClickMe),
+		// 	g.Button("I'm so cute").OnClick(onImSoCute),
+		// 	g.Button("salut").OnClick(func() { fmt.Println("yo") }),
+		// ),
+
 	}
 
-	wnd.Run(loop)
+	a := &App{
+		in:             in,
+		out:            out,
+		r:              r,
+		editor:         editor,
+		listBox:        listBox,
+		statusMsg:      statusMsg,
+		visibleWidgets: visibleWidgets,
+	}
+
+	// when an action is selected in the list
+	listBox.OnDClick(func(idx int) {
+		// defer giu.Update()
+		act := actions[idx]
+
+		out, err := act.Transform(a.out)
+		if err != nil {
+			statusMsg = "Error " + err.Error()
+			a.visibleWidgets = []g.Widget{
+				g.Row(g.Style().
+					SetColor(g.StyleColorText, color.RGBA{0x11, 0xDD, 0x11, 255}).
+					To(
+						g.Label(strings.ToUpper(a.out.Format.Name)),
+					),
+					g.Label(string(a.out.String())).Font(smallFont)),
+				listBox,
+				g.Label(statusMsg).Font(smallFont),
+			}
+			return
+		}
+		a.out = out
+		fmt.Println("action selected", act.Title(), "updated", out.String())
+
+		a.editor.Text(string(a.out.String()))
+
+		actions := r.ActionsForData(out)
+		items := make([]string, len(actions))
+		for i := 0; i < len(actions); i++ {
+			items[i] = actions[i].Title()
+		}
+		a.listBox = g.ListBox("actionList", items).Size(g.Auto, -20)
+
+		a.visibleWidgets = []g.Widget{
+			g.Row(g.Style().
+				SetColor(g.StyleColorText, color.RGBA{0x11, 0xDD, 0x11, 255}).
+				To(
+					g.Label(strings.ToUpper(a.out.Format.Name)),
+				),
+				g.Label(string(a.out.String())).Font(smallFont)),
+			listBox,
+			g.Label(statusMsg).Font(smallFont),
+		}
+	})
+
+	return a
+}
+
+func (a *App) loop() {
+	g.SingleWindow().RegisterKeyboardShortcuts(
+		// quit command
+		giu.WindowShortcut{Key: giu.KeyQ, Callback: func() {
+			if !a.disableGlobalShortcut {
+				os.Exit(0)
+			}
+		}},
+
+		// search command
+		giu.WindowShortcut{Key: giu.KeySlash, Callback: func() {
+			fmt.Println("Slash")
+			if !a.disableGlobalShortcut {
+				a.statusMsg = "Search: Type to find an action"
+				a.visibleWidgets = []g.Widget{
+					g.InputText(&a.searchInput).Hint("Type to search for an action, ESC to quit"),
+					a.listBox,
+					g.Label(a.statusMsg).Font(smallFont),
+				}
+				// giu.SetKeyboardFocusHere()
+			}
+		}},
+
+		// view command
+		giu.WindowShortcut{Key: giu.KeyV, Callback: func() {
+			if !a.disableGlobalShortcut {
+				a.statusMsg = "ESC to quit the editor"
+
+				a.editor.Text(string(a.in))
+				a.visibleWidgets = []g.Widget{
+					a.editor.Size(g.Auto, -20),
+					g.Label(a.statusMsg).Font(smallFont),
+				}
+			}
+		}},
+
+		// delete from stack command
+		giu.WindowShortcut{Key: giu.KeyBackspace, Callback: func() {
+			if !a.disableGlobalShortcut {
+				d, oa, err := a.out.Undo(a.in)
+				if err != nil { // we should not have errors in the stack
+					a.statusMsg = "Error " + err.Error()
+					return
+				}
+				a.out = d
+				a.statusMsg = "Removed action: " + oa.Title()
+				a.editor.Text(string(a.in))
+
+				actions := a.r.ActionsForData(a.out)
+				items := make([]string, len(actions))
+				for i := 0; i < len(actions); i++ {
+					items[i] = actions[i].Title()
+				}
+				a.listBox = g.ListBox("actionList", items).Size(g.Auto, -20)
+			}
+		}},
+
+		// close editor
+		giu.WindowShortcut{Key: giu.KeyEscape, Callback: func() {
+			if !a.disableGlobalShortcut {
+				os.Exit(0)
+			}
+		}},
+	).Layout(a.visibleWidgets...)
+}
+
+func main() {
+	err := clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+	input := []byte(clipboard.Read(clipboard.FmtText))
+
+	wnd := g.NewMasterWindow("OVR", 640, 480, 0)
+	app := newApp(input)
+	wnd.Run(app.loop)
 }
--- go.mod
+++ go.mod
@@ -14,19 +14,29 @@ require (
 )
 
 require (
+	github.com/AllenDang/giu v0.7.0 // indirect
+	github.com/AllenDang/go-findfont v0.0.0-20200702051237-9f180485aeb8 // indirect
+	github.com/AllenDang/imgui-go v1.12.1-0.20221124025851-59b862ca5a0c // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20231223183121-56fa3ac82ce7 // indirect
 	github.com/golang/geo v0.0.0-20230421003525-6adc56603217 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mazznoer/csscolorparser v0.1.3 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/napsy/go-css v0.0.0-20221107082635-4ed403047a64 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
@@ -37,6 +47,7 @@ require (
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/term v0.6.0 // indirect
+	gopkg.in/eapache/queue.v1 v1.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
